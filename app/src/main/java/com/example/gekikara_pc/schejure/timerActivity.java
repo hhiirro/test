e8aeb8263f7cfee44c5d7d7331d172747bac8aca
hiro <gekikara.722@gmail.com>
2018-11-02 11:44:20 +0900

    no message

---
+++ app/src/main/java/com/example/gekikara_pc/schejure/timerActivity.java
@@ -0,0 +1,13 @@
+package com.example.gekikara_pc.schejure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class timerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
